3e86cb6702238fb99012b17ad8e6f7fc15299b98
Kush Sharma <kush.sharma@peerislands.io>
2022-05-01 12:48:25 -0500

    sorted array

--- Array/Array.cs
+++ Array/Array.cs
@@ -134,5 +134,24 @@ namespace Data_Structure.Array
             }
             return sum;
         }
+
+        public static int[] sortedArray()
+        {
+            int[] nums = new int[] {1,2,3,4,5,6,7};
+            int temp=0;
+            for(int i=0; i<nums.Length-1;i++)
+            {
+                for (int j=i+1; j<nums.Length; j++)
+                {
+                    if(nums[i]>nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
     }
 }
